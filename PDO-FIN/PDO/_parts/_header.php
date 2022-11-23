6c1ac2d510d20ea90c19a3eb2247a14282eeeabd
VitorSaiter <102163159+VitorSaiter@users.noreply.github.com>
2022-11-23 15:18:16 -0400

    ...

--- PDO-FIN/PDO/_parts/_header.php
+++ PDO-FIN/PDO/_parts/_header.php
@@ -38,35 +38,6 @@ $arqui = explode(DIRECTORY_SEPARATOR,$_SERVER['PHP_SELF']);
     </div>
   </div>
 </nav>
-
-<!--
-<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" style="color:white">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">3° TI</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link <?php if($arqui[2]=='index.php')echo 'active'; ?>" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php if($arqui[2]=='clientes.php')echo 'active'; ?>" href="clientes.php">Clientes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php if($arqui[2]=='servicos.php')echo 'active'; ?>" href="servicos.php">Serviços</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php if($arqui[2]=='vendas.php')echo 'active'; ?>" href="ordens.php">Ordens de Serviço</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php if($arqui[2]=='usuarios.php')echo 'active'; ?>" href="usuarios.php">Usuários</a>
-        </li>
-    </div>
-  </div>
-</nav>
-  </nav>-->
   <!-- Navbar -->
 
   <!-- Background image -->
